c70384b8d58723e40a73e450babc93496b8b6bc3
yan <31926275+JeanRodriguez1@users.noreply.github.com>
2024-12-20 14:57:36 -0600

    2 dif import image

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 
-const icon = require("./assets/icon.png");
+//const icon = require("./assets/icon.png");
+import icon from "./assets/icon.png";
 
 export default function App() {
   return (
